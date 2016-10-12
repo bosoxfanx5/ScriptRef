837b5b629f0ec38184355192473c60f687028ce1
bosoxfanx5 <bosoxfanx5@gmail.com>
2016-10-12 00:59:40 -0700

    fixed crash

--- app/src/main/java/com/example/brooksrobison/scriptref/Main2Activity.java
+++ app/src/main/java/com/example/brooksrobison/scriptref/Main2Activity.java
@@ -3,33 +3,20 @@ package com.example.brooksrobison.scriptref;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.content.Intent;
 
 public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-        try {
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        message = "Your favorite scripture is: " + message;
 
-
-            String newString;
-
-            Bundle extras = getIntent().getExtras();
-            if (extras == null) {
-                newString = null;
-            } else {
-                newString = extras.getString("book");
-            }
-
-            TextView t = (TextView)findViewById(R.id.scriptTV);
-
-
-            t.setText(newString);
-        } catch(NullPointerException ex) {
-            
-        }
+        TextView tv = new TextView(this);
+        tv.setText(message);
+        setContentView(tv);
     }
 
-    private final Runnable mUpdateUITimerTask = new Runnable() {     public void run() {         // do whatever you want to change here, like:         t.setText("Second text to display!");     } }; private final Handler mHandler = new Handler();
 }
--- app/src/main/java/com/example/brooksrobison/scriptref/MainActivity.java
+++ app/src/main/java/com/example/brooksrobison/scriptref/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE = "scripture";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,11 +17,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void shareScript(View view) {
         Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-        EditText e = (EditText)findViewById(R.id.bookET);
+        EditText b = (EditText)findViewById(R.id.bookET);
+        EditText c = (EditText)findViewById(R.id.chaptET);
+        EditText v = (EditText)findViewById(R.id.verseET);
 
+        String message = b.getText().toString() + " " + c.getText().toString()
+                + ":" + v.getText().toString();
 
-
-        intent.putExtra("book", e.getText());
+        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
 
     }
